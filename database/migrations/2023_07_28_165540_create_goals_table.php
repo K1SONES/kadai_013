924a63a745684198c606fea8aa1a4787d234efff
K1SONES <maplekaiser7@gmail.com>
2023-07-28 17:19:16 +0900

    goalsテーブル作成

--- database/migrations/2023_07_28_165540_create_goals_table.php
+++ database/migrations/2023_07_28_165540_create_goals_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('goals', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
