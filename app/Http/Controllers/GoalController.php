9652e99fb30d51d27aa605e9c28955dcee3e6629
K1SONES <maplekaiser7@gmail.com>
2023-07-29 20:41:41 +0900

    今回はトップページしか使わないので Create,Show,Editはいらないので GoalController.phpからその項目を消した。

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -19,44 +19,12 @@ class GoalController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Goal  $goal
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Goal $goal)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Goal  $goal
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Goal $goal)
     {
         //
     }
